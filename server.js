dcc9e4d3c401823ab016029619453be55aeed45e
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-15 22:12:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,13 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
+var comments=[];
+app.get('/submit-comment',function(req,res){
+    var comment=req.query.name;
+    comments.push(comment);
+    res.send(JSON.stringify(comments));
+});
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
