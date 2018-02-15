b59265fafc1f242228fcba5f5dc55d57e4138473
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-15 21:24:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,7 @@ button.onclick=function(){
     request.open('GET','http://hetal93hasmukh.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name1=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 
 submit.onclick=function(){
@@ -38,6 +37,8 @@ submit.onclick=function(){
             }
         }
     };
+    var nameInput=document.getElementById('name');
+    var name1=nameInput.value;
     request.open('GET','http://hetal93hasmukh.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
   
