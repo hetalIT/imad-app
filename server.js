f6b0c0cb907a6c95ef99ecf5a55bb178de95e230
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-24 17:16:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ var config={
     password:'db-hetal93hasmukh-5218'
 };
 
-var pool=new Pool(config);
+/*var pool=new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('SELECT * FROM test',function(err,result){
        if(err){
@@ -96,7 +96,7 @@ app.get('/test-db',function(req,res){
          res.send(JSON.stringify(result));  
        }
    }); 
-});
+});*/
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
