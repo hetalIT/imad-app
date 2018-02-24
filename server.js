c70045c249069a914e71bcbba9d24773969bbd49
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-24 15:59:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool=require('pg').pool;
-/*var config={
+var config={
     user:'hetal93hasmukh',
     database:'hetal93hasmukh',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
-}*/
+};
 var app = express();
 app.use(morgan('combined'));
 
