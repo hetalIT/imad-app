37890082c6511cf010912772e5c40609ff80bbb2
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-24 17:20:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ var config={
 };
 
 var pool=new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test_db',function(req,res){
    pool.query('SELECT * FROM article',function(err,result){
        if(err){
          res.status(500).send(err.toString());  
