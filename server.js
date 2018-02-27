3d13309df58b569126bc6086a45726f757507c24
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-02-27 20:42:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var path = require('path');
 var Pool=require('pg').Pool;
 var app = express();
 //var crypto=require('crypto');
+var config={
+    user:'hetal93hasmukh',
+    database:'hetal93hasmukh',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'db-hetal93hasmukh-5218'
+};
 app.use(morgan('combined'));
 
-var articles={
+/*var articles={
     'article-one':{
         title:'Article one : Hetal Patel',
         heading:'Article One',
@@ -40,7 +47,7 @@ var articles={
                 <p>
                     This is the content for my Third article.
                 </p>`}
-};
+};*/
 function createTemplate(data){
     var title=data.title;
     var date=data.date;
@@ -98,7 +105,7 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-/*var pool=new Pool(config);
+var pool=new Pool(config);
 app.get('/articles/:articleName',function(req,res){
     //var articleName=req.params.articleName;
    // var articleData=
@@ -119,7 +126,7 @@ app.get('/articles/:articleName',function(req,res){
        }
    });
    
-});*/
+});
 
 
 app.get('/ui/style.css', function (req, res) {
@@ -142,15 +149,9 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 })*/
 
-var config={
-    user:'hetal93hasmukh',
-    database:'hetal93hasmukh',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:'db-hetal93hasmukh-5218'
-};
 
-var pool=new Pool(config);
+
+/*var pool=new Pool(config);
 app.get('/test_db',function(req,res){
    pool.query('SELECT * FROM test',function(err,result){
        if(err){
@@ -160,7 +161,7 @@ app.get('/test_db',function(req,res){
          res.send(JSON.stringify(result));  
        }
    }); 
-});
+});*/
 
 
 
