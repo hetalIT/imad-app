0171cd98d0bc1e03b0b078c2878895b354952563
hetalIT <31162826+hetalIT@users.noreply.github.com>
2018-03-02 23:19:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //counter code
-var button=document.getElementById('counter');
+/*var button=document.getElementById('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
@@ -14,7 +14,7 @@ button.onclick=function(){
     };
     request.open('GET','http://hetal93hasmukh.imad.hasura-app.io/counter',true);
     request.send(null);
-};
+};*/
 
 var submitCmt=document.getElementById('submit-comment');
 submitCmt.onclick=function(){
@@ -48,23 +48,28 @@ submit.onclick=function(){
         if(request.readyState==XMLHttpRequest.DONE){
             if(request.status==200)
             {
-                var names=request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++)
-                {
-                  list+='<li>'+names[i]+'</li>';  
-                
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                console.log('user logged in');
+                alert('Logged in successfully');
+            }
+            else if(request.status===403)
+            {
+                alert('Username/password is incorrect');
+            }
+            else if(request.status===500)
+            {
+                alert('Something went wrong on the server');
             }
         }
     };
-    var nameInput=document.getElementById('name');
-    var name1=nameInput.value;
-    request.open('GET','http://hetal93hasmukh.imad.hasura-app.io/submit-name?name='+name1,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    //var name1=nameInput.value;
+    
+    request.open('POST','http://hetal93hasmukh.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
   
 };
 
